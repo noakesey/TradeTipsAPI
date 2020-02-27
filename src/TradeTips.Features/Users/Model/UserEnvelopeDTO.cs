85963971dacbd9d99d7edad0d174b4bdb8e8d02f
Paul Noakes <paul.noakes@waywo.co.uk>
2020-02-27 17:14:24 +0000

    Add further

---
+++ src/TradeTips.Features/Users/Model/UserEnvelopeDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TradeTips.Features.Users
+{
+    public class UserEnvelopeDTO
+    {
+        public UserEnvelopeDTO(UserDTO user)
+        {
+            User = user;
+        }
+
+        public UserDTO User { get; set; }
+    }
+}
